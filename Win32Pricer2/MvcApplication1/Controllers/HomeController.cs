9400153c962acda50a510aefe7ed6d9d7137ea9b
andrei yannick <andrei.yannick@gmail.Com>
2015-03-20 19:08:40 +0100

    Add cpp project moteur calc

--- Win32Pricer2/MvcApplication1/Controllers/HomeController.cs
+++ Win32Pricer2/MvcApplication1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using MvcApplication1.Models;
+using MvcApplication1.ServiceReference;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,14 +17,41 @@ namespace MvcApplication1.Controllers
         {
             Panier panier = new Panier();
             List<MvcApplication1.Models.Action> ListeAction = panier.getListAction();
+
+
+
+            string[] l2 = { "BRK.A" };
+           
+            ServiceReference.DataHistoricalColumn[] hc = new ServiceReference.DataHistoricalColumn[1];
+            hc[0] = DataHistoricalColumn.Close;
+
+            DateTime debut = new DateTime(2010,1,1);
+            DateTime fin = new DateTime(2010,12,21);
+
+            ServiceReference.ActifServiceClient client = new ServiceReference.ActifServiceClient();
             
-            ViewData["Panier"] = "<table class=\"table\"><tr><th>ISIN</th><th>Nom</th><th>Devise</th><th>Prix Spot</th></tr>";
+            Data resultRq = client.getActifHistorique(l2,hc,debut,fin);
+
+            client.Close();
+
+            int tailleRes = resultRq.Ds.Tables[0].Rows.Count;
+
+            ViewData["Panier"] = "<table class=\"table\"><tr><th>Date</th><th>Prix</th></tr>";
+
+            for (int i = 0; i < tailleRes; i++)
+            {
+                ViewData["Panier"] += "<tr><td>" + resultRq.Ds.Tables[0].Rows[i]["Date"] + "</td><td>" + resultRq.Ds.Tables[0].Rows[i]["Close"] + "</td></tr>";
+            }
+
+           
+
+           /* ViewData["Panier"] = "<table class=\"table\"><tr><th>ISIN</th><th>Nom</th><th>Devise</th><th>Prix Spot</th></tr>";
 
                     foreach (MvcApplication1.Models.Action action in ListeAction)
                     {
                         ViewData["Panier"] += "<tr><td>"+  action.isin + "</td><td>" + action.nom + "</td><td>"+action.devise+"</td><td>" + action.prix + "</td></tr>";
                     } 
-                    ViewData["Panier"] +="</table>";
+                    ViewData["Panier"] +="</table>";*/
 
             return View();
         }
