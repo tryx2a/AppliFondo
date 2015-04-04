2e7ea982fad650db71fb270ab6ed200c58de5004
Francois Baton <Francois.Baton@ensimag.grenoble-inp.fr>
2015-04-04 21:54:21 +0200

    inversion pages

--- Win32Pricer2/PEPSWEB/Validation.aspx
+++ Win32Pricer2/PEPSWEB/Validation.aspx
@@ -1,8 +1,50 @@
 ﻿<%@ Page Title="Validation" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Validation.aspx.cs" Inherits="PEPSWEB.Validation" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %></h2>
-        
+    
+    <br><br>
+    <asp:Button id="sim" Text="Simuler" ForeColor="White"  BorderStyle="Ridge" Font-Bold="true" Width="150" Height="40" runat="server" BackColor="Black" OnClick="sim_Click"/>
+    <br><br>
 
+    <div id="res">
+        <div style="float:left; width:550px">
+            <asp:Label ID="prt" runat="server" Text="Prix: "></asp:Label>
+        </div>
+        <div style="float: left; width:200px">    
+            <asp:TextBox id="pr" rows="10" TextMode="multiline" runat="server" />
+        </div>
+        <br><br>
+
+        <div style="float:left; width:550px">
+            <asp:Label ID="ict" runat="server" Text="Portefeuille de Couverture: "></asp:Label>
+        </div>
+        <div style="float: left; width:300px">
+            <asp:TextBox id="intc" rows="10" TextMode="multiline" runat="server" />
+        </div>
+        <br><br>
+
+        <div style="float:left; width:550px">
+            <asp:Label ID="couvt" runat="server" Text="Part Investie au taux sans risque: "></asp:Label>
+        </div>
+        <div style="float: left; width:300px">
+            <asp:TextBox id="couv" rows="10" TextMode="multiline" runat="server" />
+        </div>
+        <br><br>
+
+
+        <asp:Chart ID="mainChart" runat="server" Visible="true" Height="500px" Width="988px" EnableViewState="True">
+            <Series>
+                <asp:Series ChartType="Line" Legend="legend" LegendText="Prix du Fondo Garantito" Name="price"></asp:Series>
+                <asp:Series ChartType="Line" Legend="legend" LegendText="Valeur du Portefeuille de Couverture" Name="portfolio"></asp:Series>
+               
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1"></asp:ChartArea>
+            </ChartAreas>
+            <Legends>
+                <asp:Legend Name="legend" BorderColor="Black"></asp:Legend>
+            </Legends>
+        </asp:Chart>
+    </div>
 </asp:Content>
 
--- Win32Pricer2/PEPSWEB/Validation.aspx.cs
+++ Win32Pricer2/PEPSWEB/Validation.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Wrapper2;
 
 namespace PEPSWEB
 {
@@ -13,5 +14,74 @@ namespace PEPSWEB
         {
 
         }
+
+        protected void sim_Click(object sender, EventArgs e)
+        {
+            double prix, ic, pnl;
+
+            WrapperClass2 wc = new WrapperClass2();
+            wc.computePnlWrapper();
+            prix = wc.getPriceWrapper();
+            ic = wc.getICWrapper();
+            pnl = wc.getPnLWrapper();
+
+            double[] payoff = wc.getPayoff();
+            double[] portfolio = wc.getPfCouverture();
+            double[] partSansRisque = wc.getPartSansRisque();
+
+            mainChart.Series["price"].Points.Clear();
+            mainChart.Series["portfolio"].Points.Clear();
+            double minportfolio = 2000;
+            double maxportfolio = 0;
+            double minprice = 2000;
+            double maxprice = 0;
+            //mainChart.Series["freeRiskInvested"].Points.Clear();
+            for (int i = 0; i < payoff.Length; i++)
+            {
+                if (payoff[i] > maxprice)
+                    maxprice = payoff[i];
+                if (payoff[i] < minprice)
+                    minprice = payoff[i];
+                mainChart.Series["price"].Points.AddXY(i, payoff[i]);
+                pr.Text += payoff[i].ToString() + " ; ";
+            }
+            for (int i = 0; i < portfolio.Length; i++)
+            {
+                if (portfolio[i] > maxportfolio)
+                    maxportfolio = portfolio[i];
+                if (portfolio[i] < minportfolio)
+                    minportfolio = portfolio[i];
+                mainChart.Series["portfolio"].Points.AddXY(i, portfolio[i]);
+                intc.Text += portfolio[i].ToString() + " ; ";
+            }
+
+            for (int i = 0; i < partSansRisque.Length; i++)
+            {
+                //mainChart.Series["freeRiskInvested"].Points.AddXY(i, partSansRisque[i]);
+                couv.Text += partSansRisque[i].ToString() + " ; ";
+            }
+
+            if (minportfolio < minprice)
+            {
+                mainChart.ChartAreas[0].AxisY.Minimum = minportfolio;
+            }
+            else
+            {
+                mainChart.ChartAreas[0].AxisY.Minimum = minprice;
+            }
+
+            if (maxportfolio < maxprice)
+            {
+                mainChart.ChartAreas[0].AxisY.Maximum = maxprice;
+            }
+            else
+            {
+                mainChart.ChartAreas[0].AxisY.Maximum = maxportfolio;
+            }
+
+
+        }
+
+
     }
 }
